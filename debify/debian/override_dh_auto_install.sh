828bb264bc8bac635376add7d0553abfcb20c467
Dan Muey <dan@cpanel.net>
2021-08-18 18:16:46 +0000

    ZC-9217: correct more variables and remove cruft

--- debify/debian/override_dh_auto_install.sh
+++ debify/debian/override_dh_auto_install.sh
@@ -27,24 +27,6 @@ install -m0644 mlogc/mlogc-default.conf $DEB_INSTALL_ROOT$_sysconfdir/mlogc.conf
 mkdir -p $DEB_INSTALL_ROOT/etc/cpanel/ea4
 echo -n $version > $DEB_INSTALL_ROOT/etc/cpanel/ea4/modsecurity.version
 
-mkdir -p debian/tmp/etc/apache2/conf.d/modsec
-mkdir -p debian/tmp/etc/apache2/conf.modules.d
-mkdir -p debian/tmp/etc/apache2/logs/modsec_audit
-mkdir -p debian/tmp/opt/cpanel/root/usr/bin
-mkdir -p debian/tmp/usr/bin
-mkdir -p debian/tmp/etc/apache2/conf.d
-mkdir -p debian/tmp/usr/lib64/apache2/modules
-
-cp debian/tmp/opt/cpanel/root/etc/apache2/conf.d/modsec/modsec2.cpanel.conf     debian/tmp/etc/apache2/conf.d/modsec/modsec2.cpanel.conf
-cp debian/tmp/opt/cpanel/root/etc/apache2/conf.d/modsec/modsec2.user.conf       debian/tmp/etc/apache2/conf.d/modsec/modsec2.user.conf
-cp debian/tmp/opt/cpanel/root/etc/apache2/conf.d/modsec2.conf                   debian/tmp/etc/apache2/conf.d/modsec/modsec2.conf
-cp debian/tmp/opt/cpanel/root/etc/apache2/conf.d/modsec2.conf                   debian/tmp/etc/apache2/conf.d/modsec2.conf
-cp debian/tmp/opt/cpanel/root/etc/apache2/conf.modules.d/800-mod_security2.conf debian/tmp/etc/apache2/conf.modules.d/800-mod_security2.conf
-cp debian/tmp/opt/cpanel/root/usr/lib64/apache2/modules/mod_security2.so        debian/tmp/usr/lib64/apache2/modules/mod_security2.so
-cp debian/tmp/opt/cpanel/root/usr/bin/mlogc                                     debian/tmp/usr/bin/mlogc
-cp debian/tmp/opt/cpanel/root/etc/mlogc.conf                                    debian/tmp/etc/mlogc.conf
-cp debian/tmp/opt/cpanel/root/usr/bin/mlogc-batch-load                          debian/tmp/usr/bin/mlogc-batch-load
-
 echo "FILELIST"
 find . -type f -print | sort
 
